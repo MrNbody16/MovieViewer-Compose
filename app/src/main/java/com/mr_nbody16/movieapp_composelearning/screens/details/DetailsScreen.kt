3ed4a9d812cdf4c0268c1a07d632d45fc57000f0
Ahmadreza Hashemi <ahmadreza13hashemi77@gmail.com>
2023-07-01 21:26:13 +0330

    finalized the course videos of movieViewer app development

--- app/src/main/java/com/mr_nbody16/movieapp_composelearning/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/mr_nbody16/movieapp_composelearning/screens/details/DetailsScreen.kt
@@ -1,21 +1,25 @@
 package com.mr_nbody16.movieapp_composelearning.screens.details
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -23,15 +27,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.mr_nbody16.movieapp_composelearning.screens.home.MainContent
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.mr_nbody16.movieapp_composelearning.model.getMovies
+import com.mr_nbody16.movieapp_composelearning.widget.MovieItemRow
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailsScreen(navController: NavController, movie: String?) {
+fun DetailsScreen(navController: NavController, movieId: String?) {
 
+    val movieFilter = getMovies().filter {
+        it.id == movieId
+    }
 
     Scaffold(
         topBar = {
@@ -49,19 +61,31 @@ fun DetailsScreen(navController: NavController, movie: String?) {
         },
     ) {
         Column(
-            modifier = Modifier.padding(it)
+            modifier = Modifier
+                .padding(it)
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Details Screen 4 $movie")
+            
+            
+            MovieItemRow(movieFilter.first())
+            
+            Spacer(modifier = Modifier.height(30.dp))
+            
+            LazyRow{
+                items(movieFilter.first().images) {image ->
+                    Card(modifier = Modifier
+                        .padding(12.dp)
+                        .size(240.dp) ,
+                    elevation = CardDefaults.cardElevation(5.dp)) {
+                        Image(painter = rememberAsyncImagePainter(ImageRequest.Builder(LocalContext.current)
+                            .data(image)
+                            .crossfade(true)
+                            .build()), contentDescription = "Movie Images" , alignment = Alignment.Center)
 
-            Spacer(modifier = Modifier.height(150.dp))
-
-            Button(onClick = {
-                navController.popBackStack()
-            }) {
-                Text(text = "Go Back")
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/mr_nbody16/movieapp_composelearning/widget/widgets.kt
+++ app/src/main/java/com/mr_nbody16/movieapp_composelearning/widget/widgets.kt
@@ -1,6 +1,6 @@
 package com.mr_nbody16.movieapp_composelearning.widget
 
-import android.text.Html.ImageGetter
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,35 +12,51 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
 import com.mr_nbody16.movieapp_composelearning.model.Movie
 import com.mr_nbody16.movieapp_composelearning.model.getMovies
+import kotlin.math.exp
 
 
 @Preview
 @Composable
 fun MovieItemRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable {
                 onItemClick.invoke(movie.id)
             },
@@ -79,7 +95,45 @@ fun MovieItemRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = 
                     text = "Released : ${movie.year}",
                     style = MaterialTheme.typography.titleMedium
                 )
+
+
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(text = buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray ,
+                            fontSize = 12.sp)) {
+                                append("Plot : ")
+
+                            }
+                            withStyle(style = SpanStyle(color = Color.DarkGray , fontSize = 13.sp ,
+                            fontWeight = FontWeight.Light)
+                            ) {
+                                append(movie.plot)
+                            }
+                        } , modifier = Modifier.padding(6.dp))
+
+                        Divider(modifier = Modifier.padding(3.dp))
+
+                        Text(text = "Director : ${movie.director}" , style = MaterialTheme.typography.bodySmall)
+                        Text(text = "Actors : ${movie.actors}" , style = MaterialTheme.typography.bodySmall)
+                        Text(text = "Rating : ${movie.rating}" , style = MaterialTheme.typography.bodySmall)
+
+                    }
+                }
+
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray
+                )
+
             }
         }
+
     }
 }
